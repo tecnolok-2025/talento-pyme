bea8237eda1b88bb99c0a9d7fec1de647a469c71
tecnolok-2025 <nestor.manucci@tecnolok.com.ar>
2026-03-05 09:49:28 -0300

    Add files via upload

--- apps/web/sw.js
+++ apps/web/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tp-v3.2.1";
+const CACHE = "tp-v3.2.2";
 const ASSETS = [
   "/", "/index.html", "/dashboard.html", "/perfil.html", "/cv.html", "/buscar.html",
   "/empleos.html", "/empresa.html", "/publicar.html", "/mis-busquedas.html",
