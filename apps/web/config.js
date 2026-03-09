c12adf1785543bc2effc8286eb655813947c5c24
tecnolok-2025 <nestor.manucci@tecnolok.com.ar>
2026-03-09 09:44:23 -0300

    Add files via upload

--- apps/web/config.js
+++ apps/web/config.js
@@ -9,5 +9,7 @@
 const TP_GLOBAL = (typeof self !== "undefined") ? self : window;
 
 TP_GLOBAL.TP_API_URL = "https://talento-pyme-api.onrender.com";
-TP_GLOBAL.TP_APP_VERSION = "5.4.1";
-TP_GLOBAL.TP_BUILD_ID = "20260308_02";
+TP_GLOBAL.TP_APP_VERSION = "5.4.2";
+TP_GLOBAL.TP_BUILD_ID = "20260309_01";
+
+TP_GLOBAL.API_BASE = TP_GLOBAL.TP_API_URL;
